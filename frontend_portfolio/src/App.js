36832478ab97e2944591e0876cc0b113c9143498
Lewis Botcherby <lewisbotcherby@gmail.com>
2023-02-28 16:15:03 +0000

    more setup

--- frontend_portfolio/src/App.js
+++ frontend_portfolio/src/App.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { About, Footer, Header, Skills, Testimonial, Work } from './containers'
+import { Navbar } from './components'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='app'>
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
+    </div>
   )
 }
 
