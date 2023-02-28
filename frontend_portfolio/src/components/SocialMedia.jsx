e68e9d6d3c84ebea828b9aaa03a417574d9ac020
Lewis Botcherby <lewisbotcherby@gmail.com>
2023-02-28 22:59:08 +0000

    changed links to open in new tab

--- frontend_portfolio/src/components/SocialMedia.jsx
+++ frontend_portfolio/src/components/SocialMedia.jsx
@@ -3,12 +3,12 @@ import { FaLinkedin, FaGithub } from 'react-icons/fa';
 
 const SocialMedia = () => (
   <div className="app__social">
-    <a href="https://www.linkedin.com/in/lewisbotcherby/">
+    <a href="https://www.linkedin.com/in/lewisbotcherby/" target="_blank" rel="noreferrer">
     <div>
       <FaLinkedin />
     </div>
     </a>
-    <a href="https://github.com/LSB616">
+    <a href="https://github.com/LSB616" target="_blank" rel="noreferrer">
     <div>
       <FaGithub />
     </div>
